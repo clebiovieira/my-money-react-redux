e2ebb99734f94011e372dd521934de56fc151dd0
Clebio <clebiovieira@gmail.com>
2017-05-28 15:36:58 -0300

    Aula107 - Estrutura com menu-sidebar-conteudo-rodape

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,15 +1,21 @@
 import '../common/template/dependencies'
 import React from 'react'
 
-/*
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
 import Footer from '../common/template/footer'
+
+/*
 import Routes from './routes'
 import Messages from '../common/msg/messages'
 */
 export default props => (
     <div className='wrapper'>
-        <h1>App</h1>
+        <Header/>
+        <SideBar/>
+        <div className='content-wrapper'>
+            <h1>Conteudo</h1>
+        </div>
+        <Footer/>
     </div>
 )
