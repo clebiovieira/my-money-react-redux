410922be4602144e168d57ebb29aefa1f22b84c8
Clebio <clebiovieira@gmail.com>
2017-05-28 19:03:48 -0300

    Aula 109 - Estrutura de layout com preparacão do dashboard

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -5,16 +5,18 @@ import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
 import Footer from '../common/template/footer'
 
-/*
 import Routes from './routes'
+
+/*
+
 import Messages from '../common/msg/messages'
 */
 export default props => (
     <div className='wrapper'>
         <Header/>
         <SideBar/>
-        <div className='content-wrapper'>
-            <h1>Conteudo</h1>
+        <div className='content-wrapper'>           
+            <Routes/>
         </div>
         <Footer/>
     </div>
