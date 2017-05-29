6dd31394623b3010b047a3bb9789121d8e755adb
Clebio <clebiovieira@gmail.com>
2017-05-28 21:19:30 -0300

    Correção da Tag <row>

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -10,16 +10,14 @@ class Dashboard extends Component{
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0'/>
                 <Content>
-                    <Row>
-
-                    
+                    <div className="row">
                         <ValueBox cols='12 4' color='green' icon='bank'
                             value='R$ 10' text='Total de Créditos'/>
                         <ValueBox cols='12 4' color='red' icon='credit-card'
                             value='R$ 8' text='Total de Débitos'/>
                         <ValueBox cols='12 4' color='blue' icon='money'
                             value='R$ 0' text='Valor Consolidado'/>                    
-                    </Row>
+                    </div>
                 </Content>
             </div>
         )
