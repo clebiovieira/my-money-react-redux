fdb87a80eb912e60402ecf7e0ab83b88730dce0e
Clebio <clebiovieira@gmail.com>
2017-06-10 13:11:04 -0300

    Aula 113  - Com correção (antes do Redux)

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -3,6 +3,7 @@ import React,{Component} from 'react'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
+import Row from  '../common/layout/row'
 
 class Dashboard extends Component{
     render(){
@@ -10,14 +11,14 @@ class Dashboard extends Component{
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0'/>
                 <Content>
-                    <div className="row">
+                    <Row>
                         <ValueBox cols='12 4' color='green' icon='bank'
                             value='R$ 10' text='Total de Créditos'/>
                         <ValueBox cols='12 4' color='red' icon='credit-card'
                             value='R$ 8' text='Total de Débitos'/>
                         <ValueBox cols='12 4' color='blue' icon='money'
                             value='R$ 0' text='Valor Consolidado'/>                    
-                    </div>
+                    </Row>
                 </Content>
             </div>
         )
