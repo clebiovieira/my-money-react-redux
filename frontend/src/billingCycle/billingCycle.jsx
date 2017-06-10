6d131057fe72909981cd884b948cc7dc24bcd4cb
Clebio <clebiovieira@gmail.com>
2017-06-10 15:34:41 -0300

    Aula 121 - Componentizando o BiilingCycle

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React,{Component} from 'react'
 
-export default props =>(
-    <div>
-        <h1>Ciclo de Pagamento</h1>
-    </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+
+
+class BillingCycle extends Component{
+    render(){
+        return(
+            <div>
+                <ContentHeader title='Ciclos de Pagamento' small='Cadastro'/>
+                <Content>
+                    Ciclos de Pagamento
+                </Content>                
+            </div>
+        )
+    }
+}
+
+export default BillingCycle
