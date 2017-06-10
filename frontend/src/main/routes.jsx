5db69e45656069f9e09af1e5f9ee9c34d22f3872
Clebio <clebiovieira@gmail.com>
2017-06-10 15:15:53 -0300

    Aula 119 - Demonstrando a Aplicação funcionando sem o uso do Redux

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -3,6 +3,7 @@ import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 
 import App from './app'
 import Dashboard from '../dashboard/dashboard'
+//import Dashboard from '../dashboard2/dashboard2'
 import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => (
